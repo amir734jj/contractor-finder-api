82df280536b5327825502ab6a3e90f03bd7eea54
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-10-26 01:03:52 -0500

    More progress

---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+namespace Models.ViewModels
+{
+    /// <summary>
+    ///     Register view model
+    /// </summary>
+    public class RegisterViewModel
+    {
+        public string Firstname { get; set; }
+        
+        public string Lastname { get; set; }
+
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
