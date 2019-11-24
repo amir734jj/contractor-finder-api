7522831540fcbcec928cf4c63836407fd511c39c
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-11-24 00:35:44 -0600

    Allow anonymous to get contractors

--- Models/Entities/Users/UserRole.cs
+++ Models/Entities/Users/UserRole.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 using Models.Enums;
 
@@ -10,22 +7,7 @@ namespace Models.Entities.Users
 {
     public class UserRole : IdentityRole<Guid>
     {
-        /// <summary>
-        ///     Website admin
-        /// </summary>
-        [JsonIgnore]
-        public uint Role { get; set; }
-
-        /// <summary>
-        ///     Roles where user belongs to
-        /// </summary>
         [NotMapped]
-        public IEnumerable<RoleEnum> Roles
-        {
-            get => Enum.GetValues(typeof(RoleEnum))
-                .Cast<RoleEnum>()
-                .Where(x => ((uint) x & Role) != 0);
-            set => Role = value.Aggregate((uint) 0, (x, acc) => x | (uint) acc);
-        }
+        public RoleEnum Role => Enum.Parse<RoleEnum>(Name);
     }
 }
