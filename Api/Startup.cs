e46fe308d358c4fdfe1abb9e2d8770aa83540ce8
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2019-12-06 13:00:11 -0600

    Minor formatting

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -123,10 +123,8 @@ namespace Api
                     new OpenApiSecurityScheme
                     {
                         Description = "JWT Authorization header using the Bearer scheme.",
-                        Type = SecuritySchemeType
-                            .Http, //We set the scheme type to http since we're using bearer authentication
-                        Scheme =
-                            "bearer" //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
+                        Type = SecuritySchemeType.Http, //We set the scheme type to http since we're using bearer authentication
+                        Scheme = "bearer" //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
                     });
             });
 
