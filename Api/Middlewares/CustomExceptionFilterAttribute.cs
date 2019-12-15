8c4c2ddae384f1acaa28d966ec65d6b852b3e7ac
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-12-15 10:36:23 -0600

    wip

---
+++ Api/Middlewares/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Middlewares
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly ILogger<CustomExceptionFilterAttribute> _logger;
+
+        public CustomExceptionFilterAttribute(ILogger<CustomExceptionFilterAttribute> logger)
+        {
+            _logger = logger;
+        }
+        
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.Demystify(), $"Caught an exception in {typeof(CustomExceptionFilterAttribute).Name}");
+            
+            base.OnException(context);
+        }
+    }
+}
