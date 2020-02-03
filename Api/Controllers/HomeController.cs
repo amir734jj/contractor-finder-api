7cde79d043d39bea4c5d694a8d1989318e200c9a
Amir <hesamian@uwm.edu>
2020-02-03 01:36:05 -0600

    Profile CRUD should be working

--- Api/Controllers/HomeController.cs
+++ Api/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
+    [AllowAnonymous]
     [Route("")]
     public class HomeController : Controller
     {
